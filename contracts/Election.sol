a2fce2e22d1cdee6ae79e7db46fc3952b4ffc244
regina8782 <49370571+regina8782@users.noreply.github.com>
2022-04-01 14:33:56 +0800

    Add files via upload

---
+++ contracts/Election.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.11;
+
+contract Election{
+    //Model a Candidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //Store accounts that have voted
+    mapping(address => bool) public voters;
+    //Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    //Store Candidates Count
+    uint public candidatesCount;
+
+    //Constructor
+    constructor()public{
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+    function addCandidate(string _name) private{
+        candidatesCount ++; //id of candidate
+        candidates[candidatesCount] = Candidate(candidatesCount, _name,0);
+    }
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender]); //account is not in voters mapping
+
+        require(_candidateId >0 && _candidateId <= candidatesCount);
+        //record that voter has voted
+        voters[msg.sender] = true;
+        //update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+    }
+}
